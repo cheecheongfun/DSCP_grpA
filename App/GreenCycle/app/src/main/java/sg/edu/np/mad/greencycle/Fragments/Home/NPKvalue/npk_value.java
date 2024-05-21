17dbb4a38a45c1a63aac76deb806bc2a6aafe785
Ern Qi <huohernqi@gmail.com>
2024-05-22 01:04:26 +0800

    NPK page Fragment set up

--- App/GreenCycle/app/src/main/java/sg/edu/np/mad/greencycle/Fragments/Home/NPKvalue/npk_value.java
+++ App/GreenCycle/app/src/main/java/sg/edu/np/mad/greencycle/Fragments/Home/NPKvalue/npk_value.java
@@ -2,12 +2,14 @@ package sg.edu.np.mad.greencycle.Fragments.Home.NPKvalue;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import sg.edu.np.mad.greencycle.R;
 
@@ -18,6 +20,54 @@ public class npk_value extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_npk_value);
-        Intent receivingEnd = getIntent();
+
+        // Get buttons from layout
+        Button btnBalanced = findViewById(R.id.btn_balanced);
+        Button btnHighNitrogen = findViewById(R.id.btn_high_nitrogen);
+        Button btnHighPhosphorous = findViewById(R.id.btn_high_phosphorous);
+        Button btnHighPotassium = findViewById(R.id.btn_high_potassium);
+
+        // Set click listeners
+        btnBalanced.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new BalancedFragment());
+            }
+        });
+
+        btnHighNitrogen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HighNitrogenFragment());
+            }
+        });
+
+        btnHighPhosphorous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HighPhosphorousFragment());
+            }
+        });
+
+        btnHighPotassium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HighPotassiumFragment());
+            }
+        });
+
+        // Load default fragment
+        loadFragment(new BalancedFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // Create fragment manager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Create fragment transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Replace the fragment on container
+        fragmentTransaction.replace(R.id.npkfragmentContainer, fragment);
+        // Commit the transaction
+        fragmentTransaction.commit();
     }
 }
