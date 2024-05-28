bb8db9c9fc060270ef6e54604506e6474afa9a0a
Ern Qi <huohernqi@gmail.com>
2024-05-29 01:25:32 +0800

    Back button in DisplayImage

--- App/GreenCycle/app/src/main/java/sg/edu/np/mad/greencycle/ImageLog/UploadImage.java
+++ App/GreenCycle/app/src/main/java/sg/edu/np/mad/greencycle/ImageLog/UploadImage.java
@@ -21,6 +21,7 @@ import androidx.core.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
