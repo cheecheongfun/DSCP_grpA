a5d98ab3e791755bbaa86d0691bb65193d48fd0d
Cheong Chun Fai <130966751+cheecheongfun@users.noreply.github.com>
2024-07-24 16:22:59 +0800

    changed functions

--- preprocess_and_append.py
+++ preprocess_and_append.py
@@ -26,74 +26,121 @@ def upload_blob(blob_name, file_path):
     with open(file_path, "rb") as data:
         blob_client.upload_blob(data, overwrite=True)
 
-def fetch_data(url, param_key, param_date, response_key, station_id, value_key):
-    params = {param_key: param_date.strftime("%Y-%m-%d")}
-    response = requests.get(url, params=params)
-    wx_forecast = response.json()
-    
-    readings = wx_forecast['items']
-    daily_values = []
-    for reading in readings:
-        for station_reading in reading['readings']:
-            if station_reading['station_id'] == station_id:
-                daily_values.append(station_reading['value'])
-    
-    if daily_values:
-        avg_value = sum(daily_values) / len(daily_values)
-        return {response_key: avg_value}
-    else:
-        return {response_key: None}
+def fetch_humidity(date):
+        params = {'date':date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/relative-humidity', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_humidity = []
+        for reading in readings:
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_humidity.append(station_reading['value'])
+        
+        if daily_humidity:
+            avg_humidity = sum(daily_humidity) / len(daily_humidity)
+            return {'date': date, 'humidity(%)': avg_humidity}
+        else:
+            return {'date': date, 'humidity(%)': None}
+
+def fetch_airtemp(date):
+        params = {'date': date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/air-temperature', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_airtemp = []
+        for reading in readings:
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_airtemp.append(station_reading['value'])
+        
+        if daily_airtemp:
+            avg_airtemp = sum(daily_airtemp) / len(daily_airtemp)
+            return {'date': date, 'air temp': avg_airtemp}
+        else:
+            return {'date': date, 'air temp': None}
+        
+def fetch_rainfall(date):
+        params = {'date': date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/rainfall', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_rainfall = []
+        for reading in readings:
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_rainfall.append(station_reading['value'])
+        
+        if daily_rainfall:
+            avg_rainfall = sum(daily_rainfall) / len(daily_rainfall)
+            return {'date': date, 'rain fall': avg_rainfall}
+        else:
+            return {'date': date, 'rain fall': None}
 
 def preprocess_weather_data(df_new):
-    def fetch_humidity(date):
-        return fetch_data('https://api.data.gov.sg/v1/environment/relative-humidity', 'date', date, 'humidity(%)', 'S50', 'value')
-
-    def fetch_airtemp(date):
-        return fetch_data('https://api.data.gov.sg/v1/environment/air-temperature', 'date', date, 'air temp', 'S50', 'value')
-
-    def fetch_rainfall(date):
-        return fetch_data('https://api.data.gov.sg/v1/environment/rainfall', 'date', date, 'rain fall', 'S50', 'value')
-
+    # r hum
     unique_dates = df_new['Date'].dt.date.unique()
-    
     with ThreadPoolExecutor(max_workers=10) as executor:
-        r_hum = list(executor.map(fetch_humidity, unique_dates))
-        airtemp = list(executor.map(fetch_airtemp, unique_dates))
-        rainfall = list(executor.map(fetch_rainfall, unique_dates))
-    
+        r_hum=list(executor.map(fetch_humidity,unique_dates))
     humidity_df = pd.DataFrame(r_hum)
+    df_new['date']=df_new['Date'].dt.date
+    df_new = pd.merge(df_new,humidity_df,on='date',how='left')
+    daily_avg=df_new.groupby('date')['humidity(%)'].mean()
+    for index, row in df_new.iterrows():
+        if pd.isnull(row['humidity(%)']):
+            date = row['date']
+            avg_humidity = daily_avg[date]
+            df_new.at[index, 'humidity(%)'] = avg_humidity
+    df_new.drop(columns=['date'],inplace=True)
+    null_dates = df_new[df_new['humidity(%)'].isnull()]['Date'].dt.date.unique()
+    min_h = df_new['humidity(%)'].min()
+    max_h = df_new['humidity(%)'].max()
+    random_h_values=np.random.uniform(min_h,max_h,size=len(null_dates))
+    for date,humidity in zip(null_dates,random_h_values):
+        df_new.loc[df_new['Date'].dt.date==date,'humidity(%)']=humidity
+    
+    # air temp
+    unique_dates=df_new['Date'].dt.date.unique()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        airtemp=list(executor.map(fetch_airtemp,unique_dates))
     airtemp_df = pd.DataFrame(airtemp)
-    rainfall_df = pd.DataFrame(rainfall)
-
-    df_new['date'] = df_new['Date'].dt.date
-    df_new = pd.merge(df_new, humidity_df, on='date', how='left')
-    df_new = pd.merge(df_new, airtemp_df, on='date', how='left')
-    df_new = pd.merge(df_new, rainfall_df, on='date', how='left')
-
-    for col in ['humidity(%)', 'air temp', 'rain fall']:
-        daily_avg = df_new.groupby('date')[col].mean()
-        for index, row in df_new.iterrows():
-            if pd.isnull(row[col]):
-                date = row['date']
-                avg_value = daily_avg[date]
-                df_new.at[index, col] = avg_value
+    df_new['date']=df_new['Date'].dt.date
+    df_new=pd.merge(df_new,airtemp_df,on='date',how='left')
+    for index,row in df_new.iterrows():
+        if pd.isnull(row['air temp']):
+            date=row['date']
+            avg_airtemp=daily_avg[date]
+            df_new.at[index,'air temp']=avg_airtemp
+    df_new.drop(columns=['date'],inplace=True)
+    null_dates=df_new[df_new['air temp'].isnull()]['Date'].dt.date.unique()
+    min_at = df_new['air temp'].min()
+    max_at = df_new['air temp'].max()
+    random_at_values=np.random.uniform(min_at,max_at,size=len(null_dates))
+    for date,airtemp in zip(null_dates,random_at_values):
+        df_new.loc[df_new['Date'].dt.date==date,'air temp']=airtemp
     
-    df_new.drop(columns=['date'], inplace=True)
-    
-    for col in ['humidity(%)', 'air temp', 'rain fall']:
-        null_dates = df_new[df_new[col].isnull()]['Date'].dt.date.unique()
-        min_val = df_new[col].min()
-        max_val = df_new[col].max()
-        random_values = np.random.uniform(min_val, max_val, size=len(null_dates))
-        for date, value in zip(null_dates, random_values):
-            df_new.loc[df_new['Date'].dt.date == date, col] = value
+    # rain fall
+    unique_dates=df_new['Date'].dt.date.unique()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        rainfall=list(executor.map(fetch_rainfall,unique_dates))
+    rainfall_df=pd.DataFrame(rainfall)
+    df_new['date']=df_new['Date'].dt.date
+    df_new=pd.merge(df_new,rainfall_df,on='date',how='left')
+    daily_avg = df_new.groupby('date')['rain fall'].mean()
+    for index,row in df_new.iterrows():
+        if pd.isnull(row['rain fall']):
+            date = row['date']
+            avg_rainfall=daily_avg[date]
+            df_new.at[index,'rain fall']=avg_rainfall
+    df_new.drop(columns=['date'],inplace=True)
+    df_new['rain fall'].fillna(0,inplace=True)
 
-    df_new['rain fall'].fillna(0, inplace=True)
-    
     return df_new
 
 def preprocess_estate_dpm(new_file):
-    # loading & preprocessing
     df_new = pd.read_excel(new_file)
     df_new['PR %'] = np.where(df_new['PR %']==0,80,df_new['PR %'])
     df_new['PR %'] = np.where(df_new['PR %']<70,70,df_new['PR %'])
@@ -164,9 +211,10 @@ def preprocess_estate_irr(new_file):
 
 def transform_df(df):
     df = df.transpose().iloc[1:]
-    df = df.iloc[:-1]
+    df=df.iloc[:-1]
     df.reset_index(inplace=True)
-    df.rename(columns={'index':'datetime',0:'kW'}, inplace=True)
+    df.rename(columns={'index':'datetime',0:'kW'},inplace=True)
+    
     return df
 
 def cap_to_2_decimals(x):
@@ -207,6 +255,7 @@ def preprocess_soe_dpm(new_file, sensorID):
     df_new.drop(columns=['Location Code'], inplace=True)
     df_new['Block'].fillna('SOE Block', inplace=True)  
     df_new.insert(1, 'Block', df_new.pop('Block'))
+
     df_new = preprocess_weather_data(df_new)
     return df_new
 
@@ -232,7 +281,6 @@ def preprocess_and_append(new_file):
     else:
         quit()
     
-
     # combine and upload
     df_combined = pd.concat([df_collated, df_new], ignore_index=True)
     df_combined.to_excel(collated_file, index=False)
