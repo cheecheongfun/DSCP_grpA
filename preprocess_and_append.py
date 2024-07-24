0f63d122fe53a7b8de4859dd90299bc36ebe6984
Cheong Chun Fai <130966751+cheecheongfun@users.noreply.github.com>
2024-07-24 16:44:31 +0800

    made changes for soe 5mins interval side

--- preprocess_and_append.py
+++ preprocess_and_append.py
@@ -27,58 +27,58 @@ def upload_blob(blob_name, file_path):
         blob_client.upload_blob(data, overwrite=True)
 
 def fetch_humidity(date):
-        params = {'date':date.strftime("%Y-%m-%d")}
-        response = requests.get('https://api.data.gov.sg/v1/environment/relative-humidity', params=params)
-        wx_forecast = response.json()
-        
-        readings = wx_forecast['items']
-        daily_humidity = []
-        for reading in readings:
-            for station_reading in reading['readings']:
-                if station_reading['station_id'] == 'S50':
-                    daily_humidity.append(station_reading['value'])
-        
-        if daily_humidity:
-            avg_humidity = sum(daily_humidity) / len(daily_humidity)
-            return {'date': date, 'humidity(%)': avg_humidity}
-        else:
-            return {'date': date, 'humidity(%)': None}
+    params = {'date':date.strftime("%Y-%m-%d")}
+    response = requests.get('https://api.data.gov.sg/v1/environment/relative-humidity', params=params)
+    wx_forecast = response.json()
+    
+    readings = wx_forecast['items']
+    daily_humidity = []
+    for reading in readings:
+        for station_reading in reading['readings']:
+            if station_reading['station_id'] == 'S50':
+                daily_humidity.append(station_reading['value'])
+    
+    if daily_humidity:
+        avg_humidity = sum(daily_humidity) / len(daily_humidity)
+        return {'date': date, 'humidity(%)': avg_humidity}
+    else:
+        return {'date': date, 'humidity(%)': None}
 
 def fetch_airtemp(date):
-        params = {'date': date.strftime("%Y-%m-%d")}
-        response = requests.get('https://api.data.gov.sg/v1/environment/air-temperature', params=params)
-        wx_forecast = response.json()
-        
-        readings = wx_forecast['items']
-        daily_airtemp = []
-        for reading in readings:
-            for station_reading in reading['readings']:
-                if station_reading['station_id'] == 'S50':
-                    daily_airtemp.append(station_reading['value'])
-        
-        if daily_airtemp:
-            avg_airtemp = sum(daily_airtemp) / len(daily_airtemp)
-            return {'date': date, 'air temp': avg_airtemp}
-        else:
-            return {'date': date, 'air temp': None}
+    params = {'date': date.strftime("%Y-%m-%d")}
+    response = requests.get('https://api.data.gov.sg/v1/environment/air-temperature', params=params)
+    wx_forecast = response.json()
+    
+    readings = wx_forecast['items']
+    daily_airtemp = []
+    for reading in readings:
+        for station_reading in reading['readings']:
+            if station_reading['station_id'] == 'S50':
+                daily_airtemp.append(station_reading['value'])
+    
+    if daily_airtemp:
+        avg_airtemp = sum(daily_airtemp) / len(daily_airtemp)
+        return {'date': date, 'air temp': avg_airtemp}
+    else:
+        return {'date': date, 'air temp': None}
         
 def fetch_rainfall(date):
-        params = {'date': date.strftime("%Y-%m-%d")}
-        response = requests.get('https://api.data.gov.sg/v1/environment/rainfall', params=params)
-        wx_forecast = response.json()
-        
-        readings = wx_forecast['items']
-        daily_rainfall = []
-        for reading in readings:
-            for station_reading in reading['readings']:
-                if station_reading['station_id'] == 'S50':
-                    daily_rainfall.append(station_reading['value'])
-        
-        if daily_rainfall:
-            avg_rainfall = sum(daily_rainfall) / len(daily_rainfall)
-            return {'date': date, 'rain fall': avg_rainfall}
-        else:
-            return {'date': date, 'rain fall': None}
+    params = {'date': date.strftime("%Y-%m-%d")}
+    response = requests.get('https://api.data.gov.sg/v1/environment/rainfall', params=params)
+    wx_forecast = response.json()
+    
+    readings = wx_forecast['items']
+    daily_rainfall = []
+    for reading in readings:
+        for station_reading in reading['readings']:
+            if station_reading['station_id'] == 'S50':
+                daily_rainfall.append(station_reading['value'])
+    
+    if daily_rainfall:
+        avg_rainfall = sum(daily_rainfall) / len(daily_rainfall)
+        return {'date': date, 'rain fall': avg_rainfall}
+    else:
+        return {'date': date, 'rain fall': None}
 
 def preprocess_weather_data(df_new):
     # r hum
@@ -259,6 +259,121 @@ def preprocess_soe_dpm(new_file, sensorID):
     df_new = preprocess_weather_data(df_new)
     return df_new
 
+def preprocess_soe_dpm_5mins(new_file,sensorID):
+    df_new = pd.read_excel(new_file)
+    df_new = transform_df(df_new)
+    df_new.replace("--",0.0,inplace=True)
+    df_new['datetime'] = pd.to_datetime(df_new['datetime'])
+    df_new['kWh'] = df_new['kW'] * (5/60)
+    if sensorID == '01':
+        df_new['Panel'] = 'M3_COM1_01'
+    else:
+        df_new['Panel'] = 'M3_COM1_02'
+    df_new['Block'] = 'SOE Block'
+
+    def fetch_humidity_2(date):
+        params = {'date': date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/relative-humidity', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_humidity = []
+        for reading in readings:
+            timestamp = reading['timestamp']
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_humidity.append({'datetime': timestamp, 'humidity(%)': station_reading['value']})
+        return daily_humidity
+
+    unique_dates = df_new['datetime'].dt.date.unique()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = list(executor.map(fetch_humidity_2, unique_dates))
+
+    r_hum = [item for sublist in results for item in sublist]
+
+    humidity_df = pd.DataFrame(r_hum)
+    humidity_df['datetime'] = pd.to_datetime(humidity_df['datetime']).dt.tz_convert(None)
+
+    df_new = pd.merge(df_new, humidity_df, on='datetime', how='left')
+
+    daily_avg = df_new.groupby(df_new['datetime'].dt.date)['humidity(%)'].mean()
+
+    for index, row in df_new.iterrows():
+        if pd.isnull(row['humidity(%)']):
+            date = row['datetime'].date()
+            avg_humidity = daily_avg[date]
+            df_new.at[index, 'humidity(%)'] = avg_humidity
+    
+    def fetch_airtemp_2(date):
+        params = {'date': date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/air-temperature', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_air_temp = []
+        for reading in readings:
+            timestamp = reading['timestamp']
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_air_temp.append({'datetime': timestamp, 'air temperature': station_reading['value']})
+        return daily_air_temp
+    unique_dates = df_new['datetime'].dt.date.unique()
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = list(executor.map(fetch_airtemp_2, unique_dates))
+
+    at_hum = [item for sublist in results for item in sublist]
+
+    airtemp_df = pd.DataFrame(at_hum)
+    airtemp_df['datetime'] = pd.to_datetime(airtemp_df['datetime']).dt.tz_convert(None)
+
+    df_new = pd.merge(df_new, airtemp_df, on='datetime', how='left')
+
+    daily_avg = df_new.groupby(df_new['datetime'].dt.date)['air temperature'].mean()
+
+    df_new['air temperature'] = df_new.apply(
+        lambda row: daily_avg[row['datetime'].date()] if pd.isnull(row['air temperature']) else row['air temperature'], 
+        axis=1
+    )
+    
+    def fetch_rainfall_2(date):
+        params = {'date': date.strftime("%Y-%m-%d")}
+        response = requests.get('https://api.data.gov.sg/v1/environment/rainfall', params=params)
+        wx_forecast = response.json()
+        
+        readings = wx_forecast['items']
+        daily_rainfall = []
+        for reading in readings:
+            timestamp = reading['timestamp']
+            for station_reading in reading['readings']:
+                if station_reading['station_id'] == 'S50':
+                    daily_rainfall.append({'datetime': timestamp, 'rainfall': station_reading['value']})
+        return daily_rainfall
+
+    unique_dates = df_new['datetime'].dt.date.unique()
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = list(executor.map(fetch_rainfall_2, unique_dates))
+
+    rf = [item for sublist in results for item in sublist]
+
+    rf_df = pd.DataFrame(rf)
+    rf_df['datetime'] = pd.to_datetime(rf_df['datetime']).dt.tz_convert(None)
+
+    df_new = pd.merge(df_new, rf_df, on='datetime', how='left')
+
+    daily_avg = df_new.groupby(df_new['datetime'].dt.date)['rainfall'].mean()
+
+    df_new['rainfall'] = df_new.apply(
+        lambda row: daily_avg[row['datetime'].date()] if pd.isnull(row['rainfall']) else row['rainfall'], 
+        axis=1
+    )
+
+    date_g = df_new.groupby(df_new['datetime'].dt.date)['kW'].sum()
+    zerokW = date_g[date_g==0].index
+    df_new.drop(df_new[df_new['datetime'].dt.date.isin(zerokW)].index, inplace=True)
+
+    return df_new
 
 def preprocess_and_append(new_file):
     try:
@@ -286,9 +401,28 @@ def preprocess_and_append(new_file):
     df_combined.to_excel(collated_file, index=False)
     upload_blob('final_data/estate_soe_combined_api_latest.xlsx', collated_file)
 
+    try:
+        collated_file = download_blob('final_data/soe_combined_api_latest.xlsx')
+    except FileNotFoundError as e:
+        print(e)
+        return
+    df_collated = pd.read_excel(collated_file)
+    if '01' in new_file:
+        df_new = preprocess_soe_dpm_5mins(new_file, '01')
+    elif '02' in new_file:
+        df_new = preprocess_soe_dpm_5mins(new_file, '02')
+    else:
+        quit()
+    
+    df_combined = pd.concat([df_collated, df_new], ignore_index=True)
+    df_combined.to_excel(collated_file, index=False)
+    upload_blob('final_data/soe_combined_api_latest.xlsx', collated_file)
+
 if __name__ == "__main__":
     import sys
     if sys.argv[1] == 'estate_soe_combined_api_latest.xlsx':
+        quit()
+    elif sys.argv[1] == 'soe_combined_api_latest.xlsx':
         quit()
     else:
         preprocess_and_append(sys.argv[1])
